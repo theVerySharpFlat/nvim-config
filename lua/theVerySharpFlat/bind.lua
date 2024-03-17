be28c7b24056266776a66b3c636f51d8a640b468
theVerySharpFlat <aidenramen29@gmail.com>
2024-03-17 16:53:38 -0500

    minor convenience changes

--- lua/theVerySharpFlat/bind.lua
+++ lua/theVerySharpFlat/bind.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 vim.keymap.set("n", "<leader>pS", vim.cmd.wa)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {remap=true})
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {remap=true})
--- lua/theVerySharpFlat/init.lua
+++ lua/theVerySharpFlat/init.lua
@@ -2,3 +2,8 @@ require("theVerySharpFlat.bind")
 require("theVerySharpFlat.packer")
 require("theVerySharpFlat.set")
 vim.cmd("colorscheme oxocarbon")
+
+vim.keymap.set("n", "<C-l>", function()
+    vim.cmd.noh()
+    vim.cmd.redraw()
+end, { silent = true })
